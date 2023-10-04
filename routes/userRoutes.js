7d7fcca7830fe089db4e60f35c4fa11f22752026
Savio Francis Mascarenhas <savio@rapidinnovation.dev>
2023-10-04 15:28:08 +0530

    minor changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,10 @@ import express from "express";
 export const userRouter = express.Router();
 import { userSignIn, userSignUp } from "../controller/login.js";
 
+userRouter.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
 //userSignIn
 userRouter.post("/signin", userSignIn);
 
